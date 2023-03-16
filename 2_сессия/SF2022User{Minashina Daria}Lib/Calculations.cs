ee69a1125e83a631690eb013e498e3d39b668ff6
Vseyopoka <88979663+Vseyopoka@users.noreply.github.com>
2023-03-16 14:20:21 +0500

    Add files via upload

---
+++ 2_сессия/SF2022User{Minashina Daria}Lib/Calculations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022User_Minashina_Daria_Lib
+{
+    public class Calculations
+    {
+        public TimeSpan[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            return startTimes;
+
+        }
+    }
+}
